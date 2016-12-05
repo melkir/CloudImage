6e3bd0c5633551260b9866e69f3f2009a353823f
Thibault Vieux <melkir13@gmail.com>
2016-12-05 17:02:16 +0100

    display image after upload

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// folder where images are uploaded
+app.use(express.static(path.join(__dirname, 'uploads')));
+
 app.use('/', index);
 app.use('/users', users);
 
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
-var upload = multer({dest: 'uploads/'});
+var upload = multer({dest: 'uploads'});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+    res.render('index', {title: 'Express', message: '', imagePath: ''});
 });
 
 router.post('/upload', upload.single('image'), function (req, res, next) {
     // req.file is the `image` file
     // req.body will hold the text fields, if there were any
     console.log(req.file);
+    res.render('index', {
+        title: 'Image uploaded',
+        message: 'File upload successfully',
+        imagePath: req.file.filename
+    });
 });
 
 module.exports = router;
