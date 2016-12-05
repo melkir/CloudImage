2f5bf9350d0bd81f7f38e569e08079e7d20ee759
Thibault Vieux <melkir13@gmail.com>
2016-12-05 15:39:50 +0100

    add multer

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.post('/profile', upload.single('image'), function (req, res, next) {
+    // req.file is the `image` file
+    // req.body will hold the text fields, if there were any
+    console.log(req.file);
 });
 
 module.exports = router;
